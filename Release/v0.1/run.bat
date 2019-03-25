af8d7f6adde77d8afcf7dcaf9542e61c21bf3856
chrismcna <chris.mcna@gmail.com>
2019-03-25 12:20:46 +0000

    Removed unnecessary files

--- Release/v0.1/run.bat
+++
@@ -1,8 +0,0 @@
-docker stop chaincoinExplorer
-docker rm chaincoinExplorer
-
-docker run --name chaincoinExplorer -p 9001:80 -d -i -t mcna/images:chaincoin-explorerv0.1-ubuntu
-#docker run -d -it --name chaincoinExplorer -p 9001:80  chaincoin-explorerv0.1-raspberry
-#docker service create -d -t --name buckled -p 9001:80  --constraint 'node.labels.swarmrole==buckled' mcna/images:buckled-raspberry
-set /p temp="Hit enter to continue"
-
--- Release/v0.1/runTest.bat
+++
@@ -1,8 +0,0 @@
-docker stop chaincoinExplorerDev
-docker rm chaincoinExplorerDev
-
-docker run --name chaincoinExplorerDev -p 9002:80 -d -i -t mcna/images:chaincoin-explorerv0.1-dev-raspberry
-#docker run -d -it --name chaincoinExplorerDev -p 9002:80  chaincoin-explorerv0.1-raspberry
-#docker service create -d -t --name buckled -p 9002:80  --constraint 'node.labels.swarmrole==buckled' mcna/images:buckled-raspberry
-set /p temp="Hit enter to continue"
-
