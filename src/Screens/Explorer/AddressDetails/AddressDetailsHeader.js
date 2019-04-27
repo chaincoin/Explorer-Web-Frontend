a7e3103bba0bdc1cf3b76ba5d79fe769dfd4e2f9
chrismcna <chris.mcna@gmail.com>
2019-04-27 15:05:25 +0100

    Implmented Address Details Header

---
+++ src/Screens/Explorer/AddressDetails/AddressDetailsHeader.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+import { Card, CardText, CardBody, CardHeader } from 'reactstrap';
+import Grid from '@material-ui/core/Grid';
+
+
+const styles = {
+  root: {
+    
+  }
+};
+
+class AddressDetailsHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  
+  }
+
+
+
+  render(){
+    const { classes, address } = this.props;
+    return (
+    <div>
+      <Card>
+        <CardHeader>
+        Address: {address.address}
+        </CardHeader>
+        <CardBody>
+          <Grid container spacing={24}>
+            <Grid item xs={12} sm={4}>
+              <div>
+                Total Sent (CHC)
+              </div>
+              <div>
+                {address.sent}
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <div>
+              Total Received (CHC)
+              </div>
+              <div>
+                {address.received}
+              </div>
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <div>
+              Balance (CHC)
+              </div>
+              <div>
+                {address.balance}
+              </div>
+            </Grid>
+            
+          </Grid>
+
+        </CardBody>
+      </Card>
+    </div>
+      
+    );
+  }
+
+  
+}
+
+
+
+AddressDetailsHeader.propTypes = {
+  classes: PropTypes.object.isRequired,
+  address: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AddressDetailsHeader);
+
+
+
+
