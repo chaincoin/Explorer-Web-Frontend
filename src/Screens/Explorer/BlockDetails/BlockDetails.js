a5fd2490686378057ffc43f4e5a70508e5c37bc5
chrismcna <chris.mcna@gmail.com>
2019-04-27 13:08:09 +0100

    Add Block Details component

---
+++ src/Screens/Explorer/BlockDetails/BlockDetails.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+import Header from './BlockDetailsHeader';
+
+import BlockchainServices from '../../../Services/BlockchainServices';
+
+
+const styles = {
+  root: {
+    
+  }
+};
+
+class BlockDetails extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        block: null,
+        error: null
+    };
+  
+    this.getBlockSubscription = null;
+  }
+
+
+  componentDidMount() {
+    const { blockId } = this.props.match.params
+
+    this.getBlockSubscription = BlockchainServices.getBlock(blockId).subscribe((block) =>{
+      this.setState({
+        block: block
+      });
+    });
+
+  }
+
+  componentWillUnmount() {
+
+    this.getBlockSubscription.unsubscribe();
+  }
+
+
+
+  render(){
+      debugger;
+    const { classes } = this.props;
+    const { block } = this.state;
+
+    if (block == null)
+    {
+      return null;
+    }
+    return (
+    <div>
+      <Header block={block}/>
+    </div>
+      
+    );
+  }
+
+  
+}
+
+BlockDetails.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(BlockDetails);
