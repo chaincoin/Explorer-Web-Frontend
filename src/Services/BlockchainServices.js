03ec9f1585ffb1ba7d6522662aa87dd3b9df3643
chrismcna <chris.mcna@gmail.com>
2019-04-29 18:34:23 +0100

    Inital WebSockets implmentation

--- src/Services/BlockchainServices.js
+++ src/Services/BlockchainServices.js
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs';
+import { Observable, Subject  } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 
 import axios from 'axios'
@@ -6,24 +6,166 @@ import Environment from './Environment';
 
 
 
+var _websocket = null;
+var websocketRequestId = 0;
+var pendingWebsocketRequests = {};
+
+var websocketMessage = new Subject();
+
+function sendWebsocketRequest(request)
+{
+    request.id = websocketRequestId;
+
+    return new Promise((resolve, reject) =>{
+      var pendingWebsocketRequest = {
+        request: request,
+        resolve: resolve,
+        reject: reject,
+        timer: setTimeout(function(){
+            reject();
+        },30000)
+      };
+      
+
+      _websocket.send(JSON.stringify(request));
+
+
+      pendingWebsocketRequests["r" + websocketRequestId] = pendingWebsocketRequest;
+      websocketRequestId++;
+    });
+}
+
+
+function sendHttpRequest(request)
+{
+  var queryParms = "?_=" + new Date().getTime();
+  for (var key in request) {
+      if (!request.hasOwnProperty(key) || key == 'op') continue;
+      queryParms = queryParms + "&" + key + "=" + request[key];
+  }
+
+  return axios.get(Environment.blockchainApiUrl + "/" + request.op + queryParms)
+  .then(res => res.data);
+}
+
+function sendRequest(request)
+{
+    if (_websocket != null) return sendWebsocketRequest(request).then((result) => result.data);
+    else return sendHttpRequest(request);
+}
+
+
+const webSocket = Observable.create(function(observer) {
+
+  observer.next(false);
+
+
+  var websocketRetryTimer = 500; 
+
+
+  var startWebsocket = () =>{
+    var tempWebsocket = new WebSocket(Environment.webServicesApiUrl);
+    // Connection opened
+
+    tempWebsocket.addEventListener('open', function (event) {
+      _websocket = tempWebsocket; 
+      websocketRequestId = 0; //reset
+      websocketRetryTimer = 500; //reset
+      pendingWebsocketRequests = {};
+      observer.next(true);
+    });
+
+    // Listen for messages
+    tempWebsocket.addEventListener('message', function (event) { //TODO: how to handle subscriptions
+      var message = JSON.parse(event.data);
+                   
+      var pendingWebsocketRequest = pendingWebsocketRequests["r" + message.id];
+      if (pendingWebsocketRequest != null)
+      {
+          if (message.success)pendingWebsocketRequest.resolve(message);
+          else pendingWebsocketRequest.reject(message);
+          clearTimeout(pendingWebsocketRequest.timer);
+          eval("delete pendingWebsocketRequests.r" + message.id);
+      }
+
+      websocketMessage.next(message);
+    });
+
+    tempWebsocket.addEventListener('close', function (event) {
+      observer.next(false);
+      _websocket = null;
+
+      for (var key in pendingWebsocketRequests) {
+        // skip loop if the property is from prototype
+        if (!pendingWebsocketRequests.hasOwnProperty(key)) continue;
+        clearTimeout(pendingWebsocketRequests[key].timer);
+        pendingWebsocketRequests[key].reject();
+      }
+
+
+
+      setTimeout(startWebsocket,websocketRetryTimer);
+      if (websocketRetryTimer < 10000) websocketRetryTimer = websocketRetryTimer * 2; //while retry is less than 10 seconds then 
+    });
+  }
+
+  startWebsocket();
+
+
+}).pipe(shareReplay({
+  bufferSize: 1,
+  refCount: false
+}));
+
+
 
 const BlockCount = Observable.create(function(observer) {
 
     var _blockCount = 0;
+    var intervalId = null;
 
-    var getBlockCountHttp = () =>{
-      axios.get(Environment.blockchainApiUrl + "/getBlockCount")
-      .then(res => res.data)
-      .then((blockCount) => {
-        if (_blockCount != blockCount) {
-          _blockCount = blockCount;
-          observer.next(blockCount);
-        }
-      });
+
+    var processBlockCount = (blockCount) => {
+      if (_blockCount != blockCount) {
+        _blockCount = blockCount;
+        observer.next(blockCount);
+      }
     };
 
-    var intervalId = setInterval(getBlockCountHttp, 30000);
-    getBlockCountHttp();
+    var getBlockCountHttp = () =>{
+      sendRequest({
+        op: "getBlockCount"
+      }).then(processBlockCount);
+    };
+    
+
+    
+
+    webSocket.subscribe(enabled =>{
+
+      if (enabled == true)
+      {
+        if (intervalId != null) clearInterval(intervalId);
+        intervalId = null;
+        sendRequest({op: "getBlockCount"}).then(processBlockCount)
+        _websocket.send(JSON.stringify({op: "newBlockSubscribe"}));//subscribe to event //TODO: should i really be using the web socket directly, if i use send request it throws an error as subcriptions are responded to
+      }
+      else
+      {
+        if (intervalId == null) {
+          intervalId = setInterval(getBlockCountHttp, 30000);
+          getBlockCountHttp();
+        }
+      }
+
+    });
+
+    
+    websocketMessage.subscribe(message =>{
+      if (message.op == "newBlock") processBlockCount(message.data.height);
+    });
+     
+    
 
     return () => {
         clearInterval(intervalId);
@@ -369,6 +511,8 @@ const BlockCount = Observable.create(function(observer) {
 
 
   export default {
+    webSocket:webSocket,
+    websocketMessage,
     blockCount: BlockCount,
     getBlock: getBlock,
     getBlocks,
