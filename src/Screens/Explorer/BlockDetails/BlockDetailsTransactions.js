b89b830067a8857fa28c87b80b86915be5948839
chrismcna <chris.mcna@gmail.com>
2019-05-08 20:16:57 +0100

    Update BlockDetailsTransactions.js

--- src/Screens/Explorer/BlockDetails/BlockDetailsTransactions.js
+++ src/Screens/Explorer/BlockDetails/BlockDetailsTransactions.js
@@ -5,7 +5,6 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
-import TableFooter from '@material-ui/core/TableFooter';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
