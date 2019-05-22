372c44c66d0495363b61c384016abdd4661ae46d
chrismcna <chris.mcna@gmail.com>
2019-05-22 12:59:49 +0100

    Coin Control Table now overflow-x scroll

---
+++ src/Screens/Explorer/MyWallet/MyAddressesSend/AddressCoinControl.js
@@ -0,0 +1,233 @@
+import React from 'react';
+
+import { combineLatest } from 'rxjs';
+
+import bigDecimal from 'js-big-decimal';
+
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+import Switch from '@material-ui/core/Switch';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TablePagination from '@material-ui/core/TablePagination';
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+
+
+import TablePaginationActions from '../../../../Components/TablePaginationActions';
+
+import MyWalletServices from '../../../../Services/MyWalletServices';
+
+const styles = {
+  tableWrapper: {
+    overflowX: 'scroll',
+    "-webkit-overflow-scrolling": "touch"
+  },
+  expansionPanelDetails:{
+    "display": "block"
+  }
+};
+
+class AddressCoinControl extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedInputs: [],
+
+      page: 0,
+      rowsPerPage: 10
+    };
+  
+    this.subscription = null;
+  }
+
+
+  componentDidMount() {
+    this.subscription = this.props.transaction.selectedInputs.subscribe(selectedInputs => this.setState({selectedInputs}));
+  }
+
+  componentWillUnmount() {
+    this.subscription.unsubscribe();
+  }
+
+
+  handleInputChange = (input) =>{
+    return (event) =>{
+
+
+      if (event.target.checked == true) this.props.transaction.addSelectedInput(input.unspent.txid, input.unspent.vout);
+      else this.props.transaction.removeSelectedInput(input.unspent.txid, input.unspent.vout);
+    }
+  };
+
+
+  handleInputAddressChange = (event) =>{
+
+    if (event.target.checked == true)this.props.inputAddress.inputs.forEach(input => this.props.transaction.addSelectedInput(input.unspent.txid, input.unspent.vout)); //TODO: maybe have an add many option
+    else this.props.inputAddress.inputs.forEach(input => this.props.transaction.removeSelectedInput(input.unspent.txid, input.unspent.vout)); //TODO: maybe have a remove many option
+  };
+
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+    
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ page: 0, rowsPerPage: parseInt(event.target.value) });
+  };
+
+  labelDisplayedRows(){
+    return "";
+  }
+
+  render = () =>
+  {
+    const { classes, inputAddress, transaction } = this.props;
+    const { selectedInputs, page, rowsPerPage } = this.state;
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, inputAddress.inputs.length - page * rowsPerPage);
+
+
+    var allSelected = inputAddress.inputs.find(input => selectedInputs.indexOf(input) == -1) == null;
+
+    
+
+
+    return (
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <Checkbox
+            checked={allSelected}
+            onClick={(e)=> e.stopPropagation()}
+            onChange={this.handleInputAddressChange}
+            color="primary"
+          />
+        {inputAddress.myAddress.name} {inputAddress.address.balance}
+        
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className={classes.expansionPanelDetails}>
+          <div className={classes.tableWrapper}>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell>Amount</TableCell>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Confirmations</TableCell>
+                  <TableCell>Locked Reason</TableCell>
+                  <TableCell></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {inputAddress.inputs.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(input => (
+                  <TableRow>
+                    <TableCell>
+                      <Checkbox
+                        checked={selectedInputs.indexOf(input) != -1}
+                        onChange={this.handleInputChange(input)}
+                        disabled={(input.disabled)}
+                        color="primary"
+                      />
+                    </TableCell>
+                    <TableCell>{input.unspent.value}</TableCell>
+                    <TableCell>{TimeToString(input.unspent.time)}</TableCell>
+                    <TableCell>{input.confirmations}</TableCell>
+                    <TableCell>
+                      { input.inMemPool ? "In MemPool, " : null }
+                      { input.inMnList ? "In MN List, " : null } 
+                      { input.isMatureCoins == false ? "Not Mature, " : null } 
+                      { input.lockState != null ? (input.lockState == true ? "User Locked," : "User Unlocked,") : null }
+                    </TableCell>
+                    <TableCell>
+
+                      {
+                        input.lockState == null ? (
+                          <div>
+                            <Button variant="contained" color="primary" onClick={(e) => MyWalletServices.addInputLockState(input.unspent.txid + "-" + input.unspent.vout, true)}>
+                              Lock
+                            </Button>
+                            <Button variant="contained" color="primary" onClick={(e) => MyWalletServices.addInputLockState(input.unspent.txid + "-" + input.unspent.vout, false)}>
+                              Unlock
+                            </Button>
+                          </div>
+
+                        ): 
+                        (
+                          <div>
+                            <Button variant="contained" color="primary" onClick={(e) => MyWalletServices.updateInputLockState(input.unspent.txid + "-" + input.unspent.vout, true)}>
+                              Lock
+                            </Button>
+                            <Button variant="contained" color="primary" onClick={(e) => MyWalletServices.updateInputLockState(input.unspent.txid + "-" + input.unspent.vout, false)}>
+                              Unlock
+                            </Button>
+                          </div>
+                        )
+                      }
+
+                      {
+                        input.lockState != null ?
+                        (
+                          <Button variant="contained" color="primary" onClick={(e) => MyWalletServices.deleteInputLockState(input.unspent.txid + "-" + input.unspent.vout)}>
+                            Clear Locked State
+                          </Button>
+                        ): null
+                      }
+                      
+
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+          <TablePagination
+            labelRowsPerPage=""
+            rowsPerPageOptions={[]}
+            colSpan={5}
+            labelDisplayedRows={this.labelDisplayedRows}
+            count={inputAddress.inputs.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            SelectProps={{
+              native: true,
+            }}
+            onChangePage={this.handleChangePage}
+            onChangeRowsPerPage={this.handleChangeRowsPerPage}
+            ActionsComponent={TablePaginationActions}
+          />
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    )
+  }
+
+}
+
+
+
+AddressCoinControl.propTypes = {
+  classes: PropTypes.object.isRequired,
+  transaction: PropTypes.object.isRequired,
+  inputAddress: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(AddressCoinControl);
+
+
+
+var TimeToString = (timestamp) =>{ //TODO: make this an include
+  var d = new Date(timestamp * 1000);
+  return d.toLocaleTimeString() + " " + d.toLocaleDateString();
+}
--- src/Screens/Explorer/MyWallet/MyAddressesSend/CoinControl.js
+++ src/Screens/Explorer/MyWallet/MyAddressesSend/CoinControl.js
@@ -2,27 +2,13 @@ import React from 'react';
 
 import { combineLatest } from 'rxjs';
 
-import bigDecimal from 'js-big-decimal';
-
-
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
 import Switch from '@material-ui/core/Switch';
 
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Checkbox from '@material-ui/core/Checkbox';
-import Button from '@material-ui/core/Button';
-
+import AddressCoinControl from './AddressCoinControl';
 
 import MyWalletServices from '../../../../Services/MyWalletServices';
 
@@ -37,7 +23,7 @@ class CoinControl extends React.Component {
     this.state = {
       coinControl: false,
       inputAddresses: [],
-      selectedInputs: []
+      selectedInputsTotal: 0
     };
   
     this.subscription = null;
@@ -45,9 +31,8 @@ class CoinControl extends React.Component {
 
 
   componentDidMount() {
-    this.subscription = combineLatest(MyWalletServices.inputAddresses, this.props.transaction.coinControl, this.props.transaction.selectedInputs,
-      this.props.transaction.selectedInputsTotal)
-      .subscribe(([inputAddresses, coinControl, selectedInputs, selectedInputsTotal]) => this.setState({coinControl, inputAddresses, selectedInputs, selectedInputsTotal}));
+    this.subscription = combineLatest(MyWalletServices.inputAddresses, this.props.transaction.coinControl, this.props.transaction.selectedInputsTotal)
+      .subscribe(([inputAddresses, coinControl, selectedInputsTotal]) => this.setState({coinControl, inputAddresses, selectedInputsTotal}));
   }
 
   componentWillUnmount() {
@@ -55,130 +40,9 @@ class CoinControl extends React.Component {
   }
 
 
-  renderInputAddressCoinControl = (inputAddress) =>
-  {
-    const { classes, transaction } = this.props;
-    const { selectedInputs } = this.state;
-
-    //var enabledInputs = inputAddress.inputs
-
-    var allSelected = inputAddress.inputs.find(input => selectedInputs.indexOf(input) == -1) == null;
-
-    const handleInputChange = (input) =>{
-      return (event) =>{
-  
-
-        if (event.target.checked == true)transaction.addSelectedInput(input.unspent.txid, input.unspent.vout);
-        else transaction.removeSelectedInput(input.unspent.txid, input.unspent.vout);
-      }
-    }
-
-
-    const handleInputAddressChange = (event) =>{
-      if (event.target.checked == true)inputAddress.inputs.forEach(input => transaction.addSelectedInput(input.unspent.txid, input.unspent.vout)); //TODO: maybe have an add many option
-      else inputAddress.inputs.forEach(input => transaction.removeSelectedInput(input.unspent.txid, input.unspent.vout)); //TODO: maybe have a remove many option
-    }
-
-
-
-    return (
-      <ExpansionPanel>
-        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-        <Checkbox
-            checked={allSelected}
-            onClick={(e)=> e.stopPropagation()}
-            onChange={handleInputAddressChange}
-            color="primary"
-          />
-        {inputAddress.myAddress.name} {inputAddress.address.balance}
-        
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Table className={classes.table}>
-            <TableHead>
-              <TableRow>
-                <TableCell></TableCell>
-                <TableCell>Amount</TableCell>
-                <TableCell>Date</TableCell>
-                <TableCell>Confirmations</TableCell>
-                <TableCell>Locked Reason</TableCell>
-                <TableCell></TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {inputAddress.inputs.map(input => (
-                <TableRow>
-                  <TableCell>
-                    <Checkbox
-                      checked={selectedInputs.indexOf(input) != -1}
-                      onChange={handleInputChange(input)}
-                      disabled={(input.disabled)}
-                      color="primary"
-                    />
-                  </TableCell>
-                  <TableCell>{input.unspent.value}</TableCell>
-                  <TableCell>{TimeToString(input.unspent.time)}</TableCell>
-                  <TableCell>{input.confirmations}</TableCell>
-                  <TableCell>
-                    { input.inMemPool ? "In MemPool, " : null }
-                    { input.inMnList ? "In MN List, " : null } 
-                    { input.isMatureCoins == false ? "Not Mature, " : null } 
-                    { input.lockState != null ? (input.lockState == true ? "User Locked," : "User Unlocked,") : null }
-                  </TableCell>
-                  <TableCell>
-
-                    {
-                      input.lockState == null ? (
-                        <div>
-                          <Button variant="contained" color="primary" onClick={(e) => MyWalletServices.addInputLockState(input.unspent.txid + "-" + input.unspent.vout, true)}>
-                            Lock
-                          </Button>
-                          <Button variant="contained" color="primary" onClick={(e) => MyWalletServices.addInputLockState(input.unspent.txid + "-" + input.unspent.vout, false)}>
-                            Unlock
-                          </Button>
-                        </div>
-
-                      ): 
-                      (
-                        <div>
-                          <Button variant="contained" color="primary" onClick={(e) => MyWalletServices.updateInputLockState(input.unspent.txid + "-" + input.unspent.vout, true)}>
-                            Lock
-                          </Button>
-                          <Button variant="contained" color="primary" onClick={(e) => MyWalletServices.updateInputLockState(input.unspent.txid + "-" + input.unspent.vout, false)}>
-                            Unlock
-                          </Button>
-                        </div>
-                      )
-                    }
-
-                    {
-                      input.lockState != null ?
-                      (
-                        <Button variant="contained" color="primary" onClick={(e) => MyWalletServices.deleteInputLockState(input.unspent.txid + "-" + input.unspent.vout)}>
-                          Clear Locked State
-                        </Button>
-                      ): null
-                    }
-                    
-
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-    )
-  }
-
-
-  handleCoinControlChange = (event ) =>{
-    ;
-  }
-
 
   render(){
-    const { classes } = this.props;
+    const { classes, transaction } = this.props;
     const { inputAddresses, coinControl, selectedInputsTotal } = this.state;
     
     return (
@@ -193,7 +57,11 @@ class CoinControl extends React.Component {
         {
           coinControl ? (
             <div> 
-              {inputAddresses.map(this.renderInputAddressCoinControl)}
+              {
+                inputAddresses.map(inputAddress =>(
+                  <AddressCoinControl inputAddress={inputAddress} transaction={transaction} />
+                ))
+              }
               <div>
                 Input Total: { selectedInputsTotal }
               </div>
@@ -210,15 +78,9 @@ class CoinControl extends React.Component {
 
 CoinControl.propTypes = {
   classes: PropTypes.object.isRequired,
-  controlledAddresses: PropTypes.object.isRequired,
-  selectedInputs: PropTypes.object.isRequired, 
+  controlledAddresses: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(CoinControl);
 
 
-
-var TimeToString = (timestamp) =>{ //TODO: make this an include
-  var d = new Date(timestamp * 1000);
-  return d.toLocaleTimeString() + " " + d.toLocaleDateString();
-}
