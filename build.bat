9d0a5123763b767fb5b9fd19c05f278fc2548146
chrismcna <chris.mcna@gmail.com>
2019-05-22 12:35:12 +0100

    updated build script

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+call npm run build
 docker build -t mcna/images:chaincoin-explorerv0.2.1-ubuntu .
 docker push mcna/images:chaincoin-explorerv0.2.1-ubuntu
 set /p temp="Hit enter to continue"
--- buildStaging.bat
+++ buildStaging.bat
@@ -1,3 +1,4 @@
+call npm run build
 docker build -t mcna/images:chaincoin-explorerv0.2.1-staging-ubuntu .
 docker push mcna/images:chaincoin-explorerv0.2.1-staging-ubuntu
 set /p temp="Hit enter to continue"
