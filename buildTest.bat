1fa92c1550d332687e264ec08f67bbe02c22cad7
chrismcna <chris.mcna@gmail.com>
2019-07-07 17:49:49 +0100

    Update buildTest.bat

--- buildTest.bat
+++ buildTest.bat
@@ -1,4 +1,4 @@
 call npm run build
-docker build -t mcna/images:chaincoin-explorerv0.2.1-dev-ubuntu .
-docker push mcna/images:chaincoin-explorerv0.2.1-dev-ubuntu
+docker build -t mcna/images:chaincoin-explorerv0.2.1.1-dev-ubuntu .
+docker push mcna/images:chaincoin-explorerv0.2.1.1-dev-ubuntu
 set /p temp="Hit enter to continue"
