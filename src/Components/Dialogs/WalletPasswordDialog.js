8cb7330ef46b5a4c1a3740c961443dd7b7f1c99f
chrismcna <chris.mcna@gmail.com>
2019-08-07 21:47:52 +0100

    Create WalletPasswordDialog.js

---
+++ src/Components/Dialogs/WalletPasswordDialog.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import FormGroup from '@material-ui/core/FormGroup';
+
+import { TextValidator, ValidatorForm} from 'react-material-ui-form-validator';
+
+import MyWalletServices from '../../Services/MyWalletServices';
+import DialogService from '../../Services/DialogService';
+
+export default (props) => {
+  const [password, setPassword] = React.useState("");
+
+
+  const form = React.useRef(null);
+
+
+  React.useEffect(() => {
+
+
+    return () => {
+    };
+  }, []);
+
+
+
+  const handleCheckWalletPassword = () =>{
+
+    form.current.isFormValid(false).then(valid =>{
+      if (valid == false) return;
+
+      if (MyWalletServices.checkWalletPassword(password))props.onClose(password);
+      else DialogService.showMessage("","Incorrect Password").subscribe();    
+      
+    });
+
+  }
+
+  return (
+    <Dialog open={true} onClose={props.onClose} aria-labelledby="form-dialog-title">
+      <DialogTitle id="form-dialog-title">Wallet Password</DialogTitle>
+      <DialogContent>
+
+        <ValidatorForm ref={form} >
+
+          <FormGroup>
+            <TextValidator
+              label="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              validators={['required']}
+              errorMessages={['required']}
+              type="password"
+            />
+          </FormGroup>
+          
+          
+          
+        
+        </ValidatorForm>
+
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => props.onClose()} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={handleCheckWalletPassword} color="primary">
+          Okay
+        </Button>
+      </DialogActions>
+    </Dialog>
+    
+  );
+}
