2c6d6b82b29fc860d2a6ffa3ffc3aaf9750cb979
chrismcna <chris.mcna@gmail.com>
2019-09-16 19:44:31 +0100

    Update ExportMyWalletData.js

--- src/Observables/ExportMyWalletData.js
+++ src/Observables/ExportMyWalletData.js
@@ -13,21 +13,27 @@ import DialogService from '../Services/DialogService';
 import Utils from '../Services/Utils';
 import WalletAction from './WalletAction';
 
-export default WalletAction(() =>{
+export default WalletAction(([encrypt, decrypt, walletPassword]) =>{
 
-    return IsWalletEncrypted.pipe(
-        switchMap(walletEncrypted => walletEncrypted == false ?
-            DialogService.showConfirmation("Export My Wallet Data","Are you sure? this export contains unencrypted private keys, please be careful"):
-            of(true)
-        ),
-        switchMap((confirm) => confirm != true ? 
-            throwError("Cancelled by user") :
-            MyWalletServices.myWalletData
-        ),
-        switchMap((config) =>{
-            Utils.stringToFileDownload("ChaincoinExplorer MyWallet.conf", JSON.stringify(config))
-            return of(null);
-        })
-    )
+    const exportData = switchMap((config) =>{
+        Utils.stringToFileDownload("ChaincoinExplorer MyWallet.conf", JSON.stringify(config))
+        return of(null);
+    });
+
+    if (encrypt == null) {
+        return DialogService.showConfirmation("Export My Wallet Data","Are you sure? this export contains unencrypted private keys, please be careful").pipe(
+            switchMap((confirm) => confirm != true ? 
+                throwError("Cancelled by user") :
+                MyWalletServices.myWalletData
+            ),
+            exportData
+        );
+    }
+    else
+    {
+        return MyWalletServices.myWalletData.pipe(exportData)
+    }
+   
+    
 })
 
